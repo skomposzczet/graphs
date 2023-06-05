1f95ef906bd38bfbb59a98b1dbecba4e3f048107
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-06-05 15:47:40 +0200

    minor changes

--- simulated-annealing/plot.py
+++ simulated-annealing/plot.py
@@ -11,8 +11,11 @@ def plot_graph(path: str):
     with open(path) as fh:
         distance = fh.readline()
 
+    plt.figure(dpi=600)
     x, y = np.loadtxt(path, skiprows=1, unpack=True)
-    plt.plot(x, y, 'g-', marker='o')
+    x = np.append(x, x[0])
+    y = np.append(y, y[0])
+    plt.plot(x, y, 'g-', marker='o', markersize=2, linewidth=1)
     plt.title(f'Distance: {distance}')
     plt.xlabel('x')
     plt.ylabel('y')
